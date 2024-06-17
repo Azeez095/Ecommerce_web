f9e183c3999bfc38f3085841d26b3ab220ff8635
AMOO AKOLADE <akoladeamoo95@gmail.com>
2024-06-17 20:39:35 +0100

    Top-Nav

---
+++ src/components/Navbar/DarkMode.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import lightPng from "../../assets/websit"
+
+const DarkMode = () => {
+  return (
+    <div>
+        <img src={lightPng} />
+    </div>
+  )
+}
+
+export default DarkMode
