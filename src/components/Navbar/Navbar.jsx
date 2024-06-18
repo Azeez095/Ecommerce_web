a799acbe3bbef2d6257f5ee7540e63950ca08a0b
AMOO AKOLADE <akoladeamoo95@gmail.com>
2024-06-18 12:33:23 +0100

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import logo from "../../assets/logo.png"
 import { IoMdSearch } from "react-icons/io";
-import { FaCartShopping } from "react-icons/fa6";
+import { FaCartShopping} from "react-icons/fa6";
+import { FaCaretDown } from "react-icons/fa";
 import DarkMode from './DarkMode';
 
 const Menu = [
@@ -97,15 +98,41 @@ const Navbar = () => {
         </div>
           
       </div>
-      <div>
-        <ul className='flex justify-around'>
+      <div className='flex justify-center'>
+        <ul className='sm:flex hidden items-center gap-4 '>
           {
             Menu.map((data)=>(
               <li key={data.id}>
-                <a href='data.link'>{data.name}</a>
+                <a 
+                  href={data.link} 
+                  className='inline-block px-4
+                  hover:text-primary duration-200'>{data.name}</a>
               </li>
-            ))
-          }
+            ))}
+            {/*Simple Dropdown and Links */}
+            <li>
+              <a 
+                href="#" 
+                className='flex items-center gap-[2px] py-2'>
+                Trending Items
+                <span>
+                <FaCaretDown 
+                className='transition-all
+                duration-200
+                group-hover: rotate-180'
+                />
+                </span>
+              </a>
+              <div>
+                <ul>
+                  {DropdownLinks.map((data=>(
+                    <li key={data.id}>
+                      <a href={data.link}>{data.name}</a>
+                    </li>
+                  )))}
+                </ul>
+              </div>
+            </li>
         </ul>
       </div>
     </div>
