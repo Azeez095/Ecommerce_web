2acba8113f8c6089030c6e97ccda76e92c465866
AMOO AKOLADE <akoladeamoo95@gmail.com>
2024-06-19 10:09:40 +0100

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -77,7 +77,12 @@ const Navbar = () => {
               group-hover:w-[300px] transition-all 
               duration-300 rounded-full border
                border-gray-300 px-2 py-1 
-               focus:outline-none focus:border-primary' />
+               focus:outline-none 
+               focus:border-1
+               focus:border-primary
+               
+               dark:border-gray-500
+               dark:bg-gray-800' />
                <IoMdSearch className='text-gray-500
                 group-hover:text-primary absolute top-1/2 -translate-y-1/2 right-3'/>
             </div>
@@ -110,11 +115,11 @@ const Navbar = () => {
               </li>
             ))}
             {/*Simple Dropdown and Links */}
-            <li>
+            <li className='group relative cursor-pointer'>
               <a 
                 href="#" 
                 className='flex items-center gap-[2px] py-2'>
-                Trending Items
+                Trending Products
                 <span>
                 <FaCaretDown 
                 className='transition-all
@@ -123,11 +128,14 @@ const Navbar = () => {
                 />
                 </span>
               </a>
-              <div>
+              <div className='absolute z-[9999] hidden 
+              group-hover:block w-[150px] rounded-md
+               bg-white p-2 text-black shadow-md'>
                 <ul>
                   {DropdownLinks.map((data=>(
                     <li key={data.id}>
-                      <a href={data.link}>{data.name}</a>
+                      <a href={data.link} className='inline-block 
+                      w-full rounded-md p-2 hover:bg-primary/20'>{data.name}</a>
                     </li>
                   )))}
                 </ul>
