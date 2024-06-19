244e7a0983631997f25ce6bb6d4e68480b4b8bed
AMOO AKOLADE <akoladeamoo95@gmail.com>
2024-06-19 21:33:42 +0100

    Hero Completed

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # React + Vite
 Create vite app: "npx create-vite@latest . --template react"
+/* Hero Section
+npm i react-slick
+npm i slick-carousel
+*/
+
+add 
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css"; to [main.jsx]
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Image1 from '../../assets/Hero/women.png'
 import Image2 from '../../assets/Hero/shopping.png'
 import Image3 from '../../assets/Hero/sale.png'
+import Slider from 'react-slick';
+ 
 
 const ImageList = [
 {
@@ -28,6 +30,19 @@ const ImageList = [
 ];
 
 const Hero = () => {
+    var settings = {
+        dots: false,
+        arrows: false,
+        infinite: true,
+        speed: 800,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        cssEase: "ease-in-out",
+        pauseOnHover: false,
+        pauseOnFocus: true,
+        };
+
   return (
     <div className='relative overflow-hidden min-h-[550px]
     sm:min-h-[650px] bg-gray-100 flex justify-center 
@@ -40,22 +55,37 @@ const Hero = () => {
         </div>
         {/*hero section*/}
         <div className='container pb-8 sm:pb-0'>
+            <Slider {...settings}>
+                {ImageList.map((data)=>(
             <div>
                 <div className='grid grid-cols-1 sm:grid-cols-2'>
-                    <div>
-                        <hi className="text-5xl sm:text-6xl
-                        lg:text-7xl font-bold">Lorem ipsum dolor sit</hi>
-                        <p className='text-sm'>Lorem ipsum dolor sit Lorem ipsum dolor sit</p>
+                    <div className='flex flex-col justify-center 
+                    gap-4 pt-12 sm:pt-0 text-center sm:text-left 
+                    order-2 sm:order-1 relative z-10'>
+                        <h1 className="text-5xl sm:text-6xl
+                        lg:text-7xl font-bold">{data.title}</h1>
+                        <p className='text-sm'>{data.description}</p>
+                    
+                        <div>
+                            <button className='bg-gradient-to-r from-primary
+                            to-secondary hover:scale-105 duration-200
+                            text-white py-1 px-4 rounded-full'>Order Now
+                            </button>
+                        </div>
                     </div>
-                    <div>
-                        <button className='bg-gradient-to-r from-primary
-                        to-secondary hover:scale-105 duration-200
-                        text-white py-1 px-4 rounded-full'>Order Now
-                        </button>
+                    <div className='order-1 sm:order-2'>
+                        <div className='relative z-10'>
+                            <img src={data.img} alt=""
+                            className='w-[300px] h-[300px] sm:h-[450px] sm:w-[450]
+                             sm:scale-125 lg: scale-120 object-contain mx-auto'
+                            />
+                        </div>
                     </div>
-
                 </div>
             </div>
+            ))}
+            </Slider>
+            
 
         </div>
     </div>
